a14f9bf532f33d9f84eb6d6f2ccc06608344c5da
sanpiyush9 <sanpiyush9@gmail.com>
2026-09-15 18:41:01 +0530

    fix: restore Next.js flat ESLint config imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript.js";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 
 export default defineConfig([
   ...nextVitals,
   ...nextTs,
-  globalIgnores([".next/**", "node_modules/**"]),
+  globalIgnores([".next/**", "node_modules/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
