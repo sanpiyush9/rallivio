579f00a9caf7ccd4425e4507a0e7620c92b0fa89
sanpiyush9 <sanpiyush9@gmail.com>
2026-09-15 17:17:17 +0530

    chore: add minimal application entrypoint

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>RALLIVIO</h1>
+      <p>Repository scaffold is ready. Product work begins from canonical specifications.</p>
+    </main>
+  );
+}
