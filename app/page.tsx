f723365a7ce709de094df1919db0b7dd027be3a6
sanpiyush9 <sanpiyush9@gmail.com>
2026-09-19 01:05:08 +0530

    Fix Discover LinkedIn icon build failure

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@
 
 import dynamic from "next/dynamic";
 import { useEffect, useMemo, useState, type PointerEvent, type CSSProperties } from "react";
-import { siDiscord, siFacebook, siInstagram, siLinkedin, siPinterest, siReddit, siSnapchat, siSpotify, siTiktok, siTwitch, siX, siYoutube } from "simple-icons";
+import { siDiscord, siFacebook, siInstagram, siPinterest, siReddit, siSnapchat, siSpotify, siTiktok, siTwitch, siX, siYoutube } from "simple-icons";
 
 const DiscoverGlobe = dynamic(() => import("../components/DiscoverGlobe"), { ssr: false });
 
 type PlatformIcon = { hex: string; path: string };
+
+const LINKEDIN_ICON: PlatformIcon = { hex: "0A66C2", path: "M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 01-2.063-2.065 2.064 2.064 0 112.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2.774 22.225 0z" };
 
 type DiscoveryItem = {
   id: string;
@@ -35,7 +37,7 @@ const platformNodes: Array<{ name: string; icon: PlatformIcon; className: string
   { name: "Instagram", icon: siInstagram, className: "instagram", state: "Coming next", position: "p2" },
   { name: "TikTok", icon: siTiktok, className: "tiktok", state: "Coming next", position: "p3" },
   { name: "X", icon: siX, className: "x", state: "Coming next", position: "p4" },
-  { name: "LinkedIn", icon: siLinkedin, className: "linkedin", state: "Coming next", position: "p5" },
+  { name: "LinkedIn", icon: LINKEDIN_ICON, className: "linkedin", state: "Coming next", position: "p5" },
   { name: "Facebook", icon: siFacebook, className: "facebook", state: "Coming next", position: "p6" },
   { name: "Twitch", icon: siTwitch, className: "twitch", state: "Coming next", position: "p7" },
   { name: "Reddit", icon: siReddit, className: "reddit", state: "Coming next", position: "p8" },
