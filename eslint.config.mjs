d12f1d5d156f986d0673d143749168ba4f756a8b
sanpiyush9 <sanpiyush9@gmail.com>
2026-09-23 19:37:35 +0530

    chore: allow approved RALLIVIO display copy in JSX

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,5 +7,10 @@ const compat = new FlatCompat({
 
 export default defineConfig([
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+    },
+  },
   globalIgnores([".next/**", "node_modules/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
